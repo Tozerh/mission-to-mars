6f79a67ea60f8790679191b0e98cf8e2ee2d9041
Tozerh <tozer.hammond@gmail.com>
2022-01-09 14:38:47 -0500

    commit after mongo practice and db mars_app setup

---
+++ app.py
@@ -0,0 +1,27 @@
+# imports tools
+from flask import Flask, rander_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+# sets up flask
+app = Flask(__name__)
+
+# uses Flask_Pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# defines route for HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+if __name__ == "__main__":
+   app.run()
